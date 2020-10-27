5ef313554b48cf58b5f716c11000b532b952dc5c
eeelkana <70084404+eeelkana@users.noreply.github.com>
2020-10-27 10:47:41 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,10 @@
 export default {
   nicknames: ["Nick", "Cewe", "Akan", "Di", "Random"],
   greetings: {
-    evening: "Good Evening",
-    afternoon: "Good Afternoon",
-    day: "Good Day",
-    morning: "Good Morning",
-    night: "Good Night"
+    morning: "Pagi",
+    day: "GBU :)",
+    afternoon: "Siang",
+    evening: "Sore",
+    night: "Malem",
   }
 };
